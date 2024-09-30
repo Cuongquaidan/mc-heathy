1f9584ece48e4cc69315af79e45bad3b1cf4a198
Cuongquaidan <121221562+Cuongquaidan@users.noreply.github.com>
2024-09-30 22:58:02 +0700

    Add banner at home page

--- mcheathy/src/app/(pages)/contact/page.tsx
+++ mcheathy/src/app/(pages)/contact/page.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-
+import { Metadata } from "next";
+export const metadata: Metadata = {
+    title: "Contact",
+    description:
+        "McHeathy is a comprehensive healthcare platform designed to offer fast, safe, and reliable health protection services. Our mission is to prioritize your well-being by providing easy access to healthcare professionals and services. With McHeathy, you can quickly book appointments with qualified doctors, schedule health consultations, and manage your medical needs with ease.Our platform is built to ensure the highest level of safety and privacy for users, enabling you to find the right care at the right time. Whether you're seeking preventative care, specialist consultations, or regular check-ups, McHeathy is here to make your healthcare experience seamless, secure, and efficient ",
+};
 function ContactPage() {
     return <div>ContactPage</div>;
 }
---
+++ mcheathy/src/components/Banner.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import React from "react";
+import banner from "@/public/assets/images/banner.png";
+import { Button } from "./ui/button";
+import { ArrowRightIcon } from "@radix-ui/react-icons";
+function Banner() {
+    return (
+        <div className="flex items-center  justify-around p-20 bg-textBlue w-[60%] mx-auto rounded-3xl dark:bg-black dark:border dark:border-t-2 dark:border-slate-500">
+            <div className="items-center text-3xl font-bold text-white">
+                <p>Book Appointment</p>
+                <p> With Trusted Doctors</p>
+                <Button className="flex gap-5 mt-5 font-bold text-black bg-white hover:bg-gray-200 hover:text-blue-600">
+                    Book appointment <ArrowRightIcon width={20} />
+                </Button>
+            </div>
+            <Image
+                src={banner}
+                alt="Banner"
+                width={500}
+                height={300}
+                className="flex-shrink"
+            />
+        </div>
+    );
+}
+
+export default Banner;
