a8bef5ea79b0da5cd78832644b475c5ab15b5f53
Hà Mạnh Cường <hamanhcuong18112004@gmail.com>
2025-02-28 11:49:55 +0700

    fix cors

--- server/server.js
+++ server/server.js
@@ -4,7 +4,15 @@ import connect from "./database/conn.js";
 import router from "./router/index.js";
 const app = express();
 app.use(express.json({ limit: "10mb" }));
-app.use(cors());
+app.use(
+    cors({
+        origin: ["https://mc-heathy.vercel.app", "http://localhost:3000"], // Chỉ cho phép frontend gọi API
+        methods: ["GET", "POST", "PUT", "PATCH", "DELETE"], // Các phương thức được phép
+        allowedHeaders: ["Content-Type", "Authorization"], // Các header được chấp nhận
+        credentials: true, // Cho phép gửi cookies nếu có
+    })
+);
+
 const PORT = process.env.PORT || 8080; 
 app.get("/", (req, res) => {
     res.status(201).json("Home GET Request");
